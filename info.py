9b912ab9402fd83f786b1bae8023235bd8786298
halo-ab <d33717154@gmail.com>
2025-04-22 16:03:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Halodd:Halodd@halodd.4bso7.mongodb.net/?retryWrites=true&w=majority&appName=Halodd")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "Halodd")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
