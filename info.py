69d7f7f703dbf9fbca4f187aaca82fa2e6465e42
halo-ab <d33717154@gmail.com>
2025-05-11 18:33:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24541460'))
 API_HASH = environ.get('API_HASH', 'c6a1afbbdca071d53ce9bb53d4602e58')
 BOT_TOKEN = environ.get('BOT_TOKEN', "7526346918:AAGwLPPfB2thuQWSzysqa6AG9EU6uhwJw6o")
