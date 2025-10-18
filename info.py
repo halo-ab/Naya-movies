95aaea031dd61b0bfa0026db99522a2dd08826d0
halo-ab <d33717154@gmail.com>
2025-10-18 13:03:27 +0530

    Restore credit comments and update URLs

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/NDT.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/1ms.jpg')).split()
 
 
 # Admins & Users
@@ -196,3 +196,4 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
